575553cb05530f40c87ffcad882996efdd68392c
Rolly Maulana Awangga <awangga@gmail.com>
2024-09-04 08:39:31 +0700

    update url

--- pagination/js/api.js
+++ pagination/js/api.js
@@ -1,7 +1,7 @@
 // js/api.js
 
 export async function fetchCategories(page = 1) {
-    const response = await fetch(`/categories?page=${page}`);
+    const response = await fetch(`https://bukupedia.alwaysdata.net/api/categories?page=${page}`);
     const data = await response.json();
     return data;
 }
